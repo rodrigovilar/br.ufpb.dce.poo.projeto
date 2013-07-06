6b2b0fec019be41a8e4dcba13deab59b1f364430
nayanneSoares <nayanne.soares@dce.ufpb.br>
2013-07-06 20:08:49 -0300

    Refatorando a classe ControladorEstoque para lançar excecoes em caso de erro

--- src/br/ufpb/dce/poo/controleestoque/controller/ControladorEstoque.java
+++ src/br/ufpb/dce/poo/controleestoque/controller/ControladorEstoque.java
@@ -3,6 +3,7 @@ package br.ufpb.dce.poo.controleestoque.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.ufpb.dce.poo.controleestoque.exception.ProdutoException;
 import br.ufpb.dce.poo.controleestoque.model.Produto;
 
 public class ControladorEstoque {
@@ -12,27 +13,51 @@ public class ControladorEstoque {
 		produtos = new ArrayList<Produto>();
 	}
 
-	public void cadastrarProduto(Produto produto) {
-		if (buscarProduto(produto.getCodigo()) == null)
-			produtos.add(produto);
+	public void cadastrarProduto(Produto produto) throws ProdutoException {
+		try {
+			if (buscarProduto(produto.getCodigo()) == null)
+				produtos.add(produto);
+		} catch (ProdutoException pe) {
+			throw new ProdutoException(
+					"Já existe um produto cadastrado com o código informado!");
+		}
 	}
 
-	public void descadastrarProduto(int codigo) {
+	public void descadastrarProduto(int codigo) throws ProdutoException {
 		Produto produto = buscarProduto(codigo);
 
 		if (produto != null)
 			produtos.remove(produto);
+
+		throw new ProdutoException(
+				"Não foi encontrado produto com o código informado!");
 	}
 
-	public Produto buscarProduto(int codigo) {
+	public Produto buscarProduto(int codigo) throws ProdutoException {
 		for (Produto produto : produtos)
 			if (produto.getCodigo() == codigo)
 				return produto;
 
-		return null;
+		throw new ProdutoException(
+				"Não foi encontrado produto com o código informado!");
 	}
 
-	public List<Produto> listarProdutos() {
-		return produtos;
+	public List<Produto> listarProdutos() throws ProdutoException {
+		if (produtos.size() > 0)
+			return produtos;
+		
+		throw new ProdutoException("Não existe nenhum produto cadastrado!");
+	}
+
+	public int getQuantidadeProduto(int codigo) throws ProdutoException{
+		Produto produto;
+		
+		try {
+			produto = buscarProduto(codigo);
+		} catch (ProdutoException pe) {
+			throw pe;
+		}
+
+		return produto.getQuantidade();
 	}
 }
