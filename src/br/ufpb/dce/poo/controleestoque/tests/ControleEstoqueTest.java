53cec7a63db3401d6abea5f0c76805913955f8b2
Gabriela Pimenta <gabriela.pimenta@dce.ufpb.br>
2013-07-06 17:37:08 -0300

    Adicionando JUnit ao Classpath e enviando a classe de testes inicial

---
+++ src/br/ufpb/dce/poo/controleestoque/tests/ControleEstoqueTest.java
@@ -0,0 +1,12 @@
+package br.ufpb.dce.poo.controleestoque.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ControleEstoqueTest {
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
