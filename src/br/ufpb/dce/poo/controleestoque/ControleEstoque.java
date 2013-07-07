810fd5ef76607d9c2eaf7ad1370c4e88d30174b5
nayanneSoares <nayanne.soares@dce.ufpb.br>
2013-07-07 18:37:32 -0300

    Update ControleEstoque.java

--- src/br/ufpb/dce/poo/controleestoque/ControleEstoque.java
+++ src/br/ufpb/dce/poo/controleestoque/ControleEstoque.java
@@ -4,6 +4,6 @@ import br.ufpb.dce.poo.controleestoque.facade.ControleEstoqueFacade;
 
 public class ControleEstoque {
 	public static void main(String[] args) {
-		ControleEstoqueFacade facade = ControleEstoqueFacade.getInstance();
+		ControleEstoqueFacade facade = new ControleEstoqueFacade();
 	}
 }
