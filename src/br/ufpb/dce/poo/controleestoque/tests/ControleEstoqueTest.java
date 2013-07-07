d1bf559415d11b9895ee9da153b70451812edef4
Gabriela Pimenta <gabriela.pimenta@dce.ufpb.br>
2013-07-07 10:19:13 -0300

    Novos testes

--- src/br/ufpb/dce/poo/controleestoque/tests/ControleEstoqueTest.java
+++ src/br/ufpb/dce/poo/controleestoque/tests/ControleEstoqueTest.java
@@ -1,5 +1,7 @@
 package br.ufpb.dce.poo.controleestoque.tests;
 
+import junit.framework.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,56 +12,70 @@ import br.ufpb.dce.poo.controleestoque.model.Produto;
 public class ControleEstoqueTest {
 	private Produto p1, p2, p3;
 	private ControleEstoqueFacade facade;
-	
+
 	@Before
 	public void setUp() throws FacadeException {
-		p1 = new Produto(1, "Notebook Ultrafino CCE com Intel Dual Core 2GB 500GB LED 14\" Preto Windows 8", 10, 899.00f);
-		p2 = new Produto(2, "Geladeira / Refrigerador Brastemp Frost Free Ative Inverse BRE50 Inox 422 Litros", 5, 2699.00f);
-		p3 = new Produto(3, "Smart TV 3D LED 47\" LG 47LA6200 FULL HD 3 HDMI 3 USB Wi-fi 120Hz + 4 Óculos 3D", 1, 2969.10f);
+		p1 = new Produto(
+				1,
+				"Notebook Ultrafino CCE com Intel Dual Core 2GB 500GB LED 14\" Preto Windows 8",
+				10, 899.00f);
+		p2 = new Produto(
+				2,
+				"Geladeira / Refrigerador Brastemp Frost Free Ative Inverse BRE50 Inox 422 Litros",
+				5, 2699.00f);
+		p3 = new Produto(
+				3,
+				"Smart TV 3D LED 47\" LG 47LA6200 FULL HD 3 HDMI 3 USB Wi-fi 120Hz + 4 Óculos 3D",
+				1, 2969.10f);
 		
 		facade = ControleEstoqueFacade.getInstance();
 		facade.cadastrarProduto(p1);
 		facade.cadastrarProduto(p2);
 		facade.cadastrarProduto(p3);
 	}
-	
+
 	@Test
-	public void testCadastrarProduto() {
-		
+	public void testCadastrarProduto() throws FacadeException {
+		Produto p4 = new Produto(
+				4,
+				"Smartphone Samsung Galaxy Ace Preto, Desbloqueado Vivo, GSM, Android, Câmera de 5MP, Tela Touchscreen 3.5\", 3G, Wi-Fi, Bluetooth e Cartão de Memória 2GB",
+				3, 499.00f);
+		facade.cadastrarProduto(p4);
+		Assert.assertEquals(facade.listarProdutos().size(), 4);
 	}
-	
+
 	@Test
 	public void testBuscarProduto() {
-		
+
 	}
-	
+
 	@Test
 	public void testListarProdutos() {
-		
+
 	}
-	
+
 	@Test
 	public void testGetQuantidadeProduto() {
-		
+
 	}
-	
+
 	@Test
 	public void testReporProduto() {
-		
+
 	}
-	
+
 	@Test
 	public void testRetirarProduto() {
-		
+
 	}
-	
+
 	@Test
 	public void testValorTotalEmEstoque() {
-		
+
 	}
-	
+
 	@Test
 	public void testValorTotalProduto() {
-		
+
 	}
 }
