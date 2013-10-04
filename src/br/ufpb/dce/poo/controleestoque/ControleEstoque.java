de896c008f5fe32af109605464c6d2ed3375dad3
nayanneSoares <nayanne.soares@dce.ufpb.br>
2013-10-03 21:39:34 -0300

    Delete ControleEstoque.java

--- src/br/ufpb/dce/poo/controleestoque/ControleEstoque.java
+++
@@ -1,9 +0,0 @@
-package br.ufpb.dce.poo.controleestoque;
-
-import br.ufpb.dce.poo.controleestoque.facade.ControleEstoqueFacade;
-
-public class ControleEstoque {
-	public static void main(String[] args) {
-		ControleEstoqueFacade facade = new ControleEstoqueFacade();
-	}
-}
